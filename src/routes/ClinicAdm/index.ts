b91b2f96b5234ff0406056e2bdcd9e0017c957a3
Geovane Filho <geo.vane.20002@gmail.com>
2023-01-19 19:24:01 -0300

    fix: update merge

--- src/routes/ClinicAdm/index.ts
+++ src/routes/ClinicAdm/index.ts
@@ -8,5 +8,6 @@ routes.get('/clinic-adm', new ClinicAdministratorsController().listAll);
 routes.get('/clinic-adm/:param', new ClinicAdministratorsController().listOne);
 routes.get('/clinic-adm/clinic/:clinic_id', new ClinicAdministratorsController().listAllByClinicID);
 routes.delete('/clinic-adm/:clinicAdm_id', new ClinicAdministratorsController().delete);
+routes.put('/clinic-adm/:clinicAdministrator_id', new ClinicAdministratorsController().update);
 
 export { routes as ClinicAdmRoutes };
--- src/routes/Coordinators/index.ts
+++ src/routes/Coordinators/index.ts
@@ -6,5 +6,6 @@ const routes = Router();
 routes.post('/coordinators', new CoordinatorController().create);
 routes.get('/coordinators/:clinic_id', new CoordinatorController().listAll);
 routes.delete('/coordinators/:coordinator_id', new CoordinatorController().delete);
+routes.put('/coordinators/:coordinator_id', new CoordinatorController().update);
 
 export { routes as CoordinatorsRoutes };
