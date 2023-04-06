4893763aa45e80554cd09e6802624457bebac6ce
guisnunex <guilhermesouza96910524@gmail.com>
2023-04-06 11:51:28 -0300

    chore: add clinic adm in clinic listing

--- src/services/ClinicServices/ListAllClinicService.ts
+++ src/services/ClinicServices/ListAllClinicService.ts
@@ -1,14 +1,22 @@
 import { getRepository } from 'typeorm';
 import { Clinic } from '../../entities/ClinicEntitie';
+import { ClinicAdministrator } from '../../entities/ClinicAdministratorEntitie';
 
 export class ListAllClinicsService {
-  async execute(): Promise<Clinic[] | Error> {
+  async execute(): Promise<Clinic[] | Error | boolean> {
     const repo = getRepository(Clinic);
+    const repoClinicAdm = getRepository(ClinicAdministrator);
 
     try {
       const clinics = await repo.find({ relations: ['address'] });
+      const clinicAdms = await repoClinicAdm.find();
 
-      return clinics;
+      const clinicsWithAdms = clinics.map(clinic => {
+        const adms = clinicAdms.filter(adm => adm.clinic_id === clinic.id);
+        return { ...clinic, administrator: adms[0] };
+      });
+
+      return clinicsWithAdms;
     } catch (error: any) {
       return new Error(error);
     }
